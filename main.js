488a8d620ebfa25523c21795dae798713b2b3b57
ioliinyk84 <ioliinyk84@gmail.com>
2018-10-27 11:46:24 +0300

    create empty components

---
+++ main.js
@@ -0,0 +1,26 @@
+const clientList = React.createClass({
+
+});
+
+const clientNote = React.createClass({
+
+});
+
+
+const clientSearch = React.createClass({
+
+});
+
+const detailInfo = React.createClass({
+
+});
+
+const listApp = React.createClass({
+
+});
+
+
+ReactDOM.render (
+    <listApp />,
+    document.getElementById('root')
+);
